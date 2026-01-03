e79c1914d39c29188775c05f36bfbcb0f89b98f7
daksh <akshd@outlook.com>
2026-01-03 16:20:20 +0530

    feat: add menu bar

---
+++ supatypa/StatusBarController.swift .swift
@@ -0,0 +1,24 @@
+import AppKit
+
+class StatusBarController {
+    private var statusItem: NSStatusItem
+
+    init() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+
+        if let button = statusItem.button {
+            button.title = "⌨️"
+        }
+
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Today: 0 chars", action: nil, keyEquivalent: ""))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(quit), keyEquivalent: "q"))
+
+        statusItem.menu = menu
+    }
+
+    @objc func quit() {
+        NSApplication.shared.terminate(nil)
+    }
+}
