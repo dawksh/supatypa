a906ae4e6a5f43a568ca699302b2621474250b05
daksh <akshd@outlook.com>
2026-01-03 16:12:02 +0530

    Initial Commit

---
+++ supatypa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  supatypa
+//
+//  Created by Daksh on 03/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ supatypa/supatypaApp.swift
@@ -0,0 +1,17 @@
+//
+//  supatypaApp.swift
+//  supatypa
+//
+//  Created by Daksh on 03/01/26.
+//
+
+import SwiftUI
+
+@main
+struct supatypaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
